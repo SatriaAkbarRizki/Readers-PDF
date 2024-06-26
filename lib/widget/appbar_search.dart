fd7377fe1425e00fa8bb8cd1012dffe48989eeab
SatriaAkbarRizki <sartiapl@gmail.com>
2024-06-26 10:56:02 +0700

    fix: input search text not real time

--- lib/widget/appbar_search.dart
+++ lib/widget/appbar_search.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pdfrx/pdfrx.dart';
@@ -34,24 +36,26 @@ class AppBarSearch extends StatelessWidget {
             padding:
                 const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 20),
             child: SizedBox(
-                height: 50,
-                width: 50,
+              height: 50,
+              width: 50,
+              child: BlocListener<PdfBloc, PdfState>(
+                listener: (context, state) {
+                  if (state is PdfSearchingText) {
+                    pdfTextSearcher.startTextSearch(state.text.toString(),
+                        caseInsensitive: true, goToFirstMatch: true);
+                    if (state.text!.isEmpty) {
+                      pdfTextSearcher.resetTextSearch();
+                    }
+                  }
+                },
                 child: TextFormField(
                   controller: textEditingController,
                   onChanged: (value) {
                     context.read<PdfBloc>().add(OnPdfSearchingText(value));
-
-                    final myState = context.read<PdfBloc>().state;
-
-                    if (myState is PdfSearchingText) {
-                      pdfTextSearcher.startTextSearch(myState.text.toString(),
-                          caseInsensitive: true, goToFirstMatch: true);
-                      if (value.isEmpty) {
-                        pdfTextSearcher.resetTextSearch();
-                      }
-                    }
                   },
-                )),
+                ),
+              ),
+            ),
           ),
         ),
         IconButton(
