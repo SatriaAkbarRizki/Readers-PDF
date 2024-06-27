1120057feb38e042bcddfd1ce9178ca6184dd5d1
SatriaAkbarRizki <sartiapl@gmail.com>
2024-06-27 12:26:19 +0700

    change: Fising The ParentDataWidget Expanded

--- lib/widget/currentpdf.dart
+++ lib/widget/currentpdf.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:simplereader/widget/dialog_doc.dart';
 
@@ -7,56 +6,60 @@ class CurrentPDF extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.builder(
-        physics: const BouncingScrollPhysics(),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 2,
-            childAspectRatio: 0.7,
-            mainAxisSpacing: 2),
-        itemCount: 10,
-        itemBuilder: (context, index) => GestureDetector(
-          onLongPress: () => showDialog(
-            useSafeArea: true,
-            context: context,
-            builder: (context) => const DialogDoc(),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 250,
-                    width: 250,
-                    decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage('assets/image/sample-book.jpg'),
-                            fit: BoxFit.contain),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black38,
-                              offset: Offset(0, 12.0),
-                              blurRadius: 10),
-                        ],
-                        borderRadius: BorderRadius.circular(5)),
-                  ),
+    return CustomScrollView(
+      slivers: [
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 2,
+              childAspectRatio: 0.7,
+              mainAxisSpacing: 2),
+          delegate: SliverChildBuilderDelegate(
+            childCount: 10,
+            (context, index) => GestureDetector(
+              onLongPress: () => showDialog(
+                useSafeArea: true,
+                context: context,
+                builder: (context) => const DialogDoc(),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 250,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image:
+                                    AssetImage('assets/image/sample-book.jpg'),
+                                fit: BoxFit.contain),
+                            boxShadow: const [
+                              BoxShadow(
+                                  color: Colors.black38,
+                                  offset: Offset(0, 12.0),
+                                  blurRadius: 10),
+                            ],
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: Text(
+                        'The book of people San francisco',
+                        style: Theme.of(context).textTheme.labelSmall,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: Text(
-                    'The book of people San francisco',
-                    style: Theme.of(context).textTheme.labelSmall,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                )
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
