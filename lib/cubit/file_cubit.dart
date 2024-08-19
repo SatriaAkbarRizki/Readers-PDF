c4148570242713af58a456aa92b222bfe7e0786d
SatriaAkbarRizki <sartiapl@gmail.com>
2024-08-19 22:19:49 +0700

    update: integrate File service using cubit

--- lib/cubit/file_cubit.dart
+++ lib/cubit/file_cubit.dart
@@ -1,12 +1,41 @@
+import 'dart:developer';
 import 'dart:io';
 import 'package:bloc/bloc.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:simplereader/service/filedoc.dart';
 
-class FileCubit extends Cubit<File?> {
+import '../screens/readingpdf.dart';
+
+class FileCubit extends Cubit<dynamic> {
   FileCubit() : super(null);
   ServiceFile serviceFile = ServiceFile();
 
-  void getFile() async => await serviceFile.getFileDoc().then(
-        (value) => emit(value),
+  void getFile(BuildContext context) async =>
+      await serviceFile.getFileDoc().then(
+        (results) {
+          if (results != null) {
+            final isPdf = results.files.first.name.endsWith('pdf');
+            if (isPdf) {
+              context.go(ReadPDFScreens.routeName, extra: results);
+            } else {
+              SnackBar snackBar =
+                  const SnackBar(content: Text('This not documents'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              log('Not Pdf');
+            }
+          }
+          emit(results);
+        },
+      );
+
+  void findPdf() async => await serviceFile.findPDFAll().then(
+        (value) {
+          for (var element in value) {
+            log('PDF found: $element');
+          }
+          emit(value);
+        },
       );
 }
--- lib/service/filedoc.dart
+++ lib/service/filedoc.dart
@@ -1,14 +1,41 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:simplereader/service/permission.dart';
 
 class ServiceFile {
-  Future<File?> getFileDoc() async {
+  List<FileSystemEntity> rootDirectory = [];
+  List<String> listPDF = [];
+  Future<FilePickerResult?> getFileDoc() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null) {
-      File file = File(result.files.single.path!);
-      return file;
+      return result;
     }
     return null;
+  }
+
+  Future<List<String>> findPDFAll() async {
+    listPDF.clear();
+    if (await requestPermission()) {
+      try {
+        Directory dir = Directory('/storage/emulated/0');
+        rootDirectory = dir.listSync();
+
+        for (var elementDir in rootDirectory) {
+          if (FileSystemEntity.isDirectorySync(elementDir.path)) {
+            Directory(elementDir.path).listSync().forEach((elementDirInside) {
+              if (elementDirInside.path.endsWith('.pdf')) {
+                
+                listPDF.add(elementDirInside.path);
+              }
+            });
+          }
+        }
+      } catch (e) {
+        log('Error: $e');
+      }
+    }
+    return listPDF;
   }
 }
