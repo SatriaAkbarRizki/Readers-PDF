1e8f3f10b52e8c0a1a4a687fa08718c2a294398f
SatriaAkbarRizki <sartiapl@gmail.com>
2024-08-19 22:21:04 +0700

    update: efficient code

--- lib/widget/floating_pdf.dart
+++ lib/widget/floating_pdf.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:simplereader/screens/readingpdf.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simplereader/cubit/file_cubit.dart';
 
 class FloatingPdfAction extends StatelessWidget {
   const FloatingPdfAction({super.key});
@@ -11,20 +9,8 @@ class FloatingPdfAction extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () async {
-        final results = await FilePicker.platform.pickFiles(type: FileType.any);
-
-        if (results != null && context.mounted) {
-          PlatformFile file = results.files.first;
-          final isPdf = file.name.endsWith('pdf');
-          if (isPdf) {
-            context.go(ReadPDFScreens.routeName, extra: results);
-          } else {
-            SnackBar snackBar =
-                const SnackBar(content: Text('This not documents'));
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            log('Not Pdf');
-          }
-        }
+        // context.read<FileCubit>().findPdf();
+        context.read<FileCubit>().getFile(context);
       },
       child: const Icon(Icons.add),
     );
