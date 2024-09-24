3b93d596ff9416f627dbd8a1589c5b4b589ecc1c
SatriaAkbarRizki <sartiapl@gmail.com>
2024-09-24 10:32:00 +0700

    add: listen change color on floating

--- lib/widget/floating_pdf.dart
+++ lib/widget/floating_pdf.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:simplereader/bloc/pdf/pdf_bloc.dart';
+import 'package:simplereader/cubit/theme_cubit.dart';
+import 'package:simplereader/model/thememodel.dart';
 
 class FloatingPdfAction extends StatelessWidget {
   const FloatingPdfAction({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () async {
-        // context.read<FileCubit>().findPdf();
-        // context.read<FileCubit>().getFile(context);
+    return BlocBuilder<ThemeCubit, Thememodel>(
+      builder: (context, state) {
+        return FloatingActionButton(
+          backgroundColor: state.widget,
+          onPressed: () async {
+            // context.read<FileCubit>().findPdf();
+            // context.read<FileCubit>().getFile(context);
 
-        context.read<PdfBloc>().add(OnPdfOpenFile(context: context));
+            context.read<PdfBloc>().add(OnPdfOpenFile(context: context));
+          },
+          child: Icon(
+            Icons.add,
+            color: state.text,
+          ),
+        );
       },
-      child: const Icon(Icons.add),
     );
   }
 }
