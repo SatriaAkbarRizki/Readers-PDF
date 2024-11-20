37e8db910a2878b8f3154ed300be2238f7e61d0f
SatriaAkbarRizki <sartiapl@gmail.com>
2024-11-20 18:57:24 +0700

    update: integrate cubit color picker and slider opacity

---
+++ lib/cubit/color_picker.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ColorPickerCubit extends Cubit<Color> {
+  ColorPickerCubit() : super(Colors.red);
+
+  Color pickerColor = Colors.red;
+  Color currentColor = Color(0xff443a49);
+
+  void changeColor(Color colors) {
+    pickerColor = colors;
+    emit(colors);
+  }
+}
--- lib/cubit/slider_level.dart
+++ lib/cubit/slider_level.dart
@@ -5,11 +5,15 @@ class SliderCubit extends Cubit<double> {
 
   double valueQuality = 0.0;
   double valueScale = 0.0;
+  double valueOpacity = 0.0;
 
   void changeValueQuality(double valueSlider) =>
       {valueQuality = valueSlider, emit(valueSlider)};
   void changeValueScale(double valueSlider) =>
       {valueScale = valueSlider, emit(valueSlider)};
+
+    void changeValueOpacity(double valueSlider) =>
+      {valueOpacity = valueSlider, emit(valueSlider)};
 
   void reset() => {emit(valueQuality = 100.0), emit(valueScale = 1.0)};
 }
