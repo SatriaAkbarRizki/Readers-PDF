b8acadabc5c0277006d9b46acda37981e6f200d3
SatriaAkbarRizki <sartiapl@gmail.com>
2024-11-28 12:16:43 +0700

    update: remove overlay

--- lib/navigation/navbar.dart
+++ lib/navigation/navbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:simplereader/bloc/tools_pdf/cubit/channel_home.dart';
 import 'package:simplereader/cubit/navbar_cubit.dart';
 import 'package:simplereader/cubit/theme_cubit.dart';
 import 'package:simplereader/model/thememodel.dart';
@@ -19,52 +20,67 @@ class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final navCubit = context.read<NavbarCubit>();
-    return Scaffold(
-      body: BlocBuilder<NavbarCubit, int>(
-        builder: (context, state) {
-          return listtNavBar[state];
-        },
-      ),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(10), topRight: Radius.circular(15)),
-        child: BlocBuilder<ThemeCubit, Thememodel>(
-          builder: (context, themes) {
-            return BottomNavigationBar(
-                currentIndex: navCubit.state,
-                onTap: (value) => navCubit.switchNavBar(value),
-                backgroundColor: themes.background,
-                selectedItemColor: themes.text,
-                unselectedItemColor: themes.widget,
-                items: [
-                  BottomNavigationBarItem(
-                      label: 'Home',
-                      icon: SvgPicture.asset(
-                        'assets/icons/open-book.svg',
-                        colorFilter:
-                            ColorFilter.mode(themes.widget, BlendMode.srcIn),
-                      )),
-                  // BottomNavigationBarItem(
-                  //     label: 'Listen',
-                  //     icon: SvgPicture.asset(
-                  //       'assets/icons/listen.svg',
-                  //       colorFilter: const ColorFilter.mode(
-                  //           Color(0xff1b5ed1), BlendMode.srcIn),
-                  //     )),
-                  BottomNavigationBarItem(
-                      label: 'Profile',
-                      icon: SvgPicture.asset(
-                        'assets/icons/account.svg',
-                        colorFilter:
-                            ColorFilter.mode(themes.widget, BlendMode.srcIn),
-                      ))
-                ]);
+    return BlocProvider(
+      create: (context) => ChannelHome(),
+      child: Scaffold(
+        body: BlocBuilder<NavbarCubit, int>(
+          builder: (context, state) {
+            return listtNavBar[state];
           },
         ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10), topRight: Radius.circular(15)),
+          child: BlocBuilder<ThemeCubit, Thememodel>(
+            builder: (context, themes) {
+              return Theme(
+                data: Theme.of(context).copyWith(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                ),
+                child: BottomNavigationBar(
+                    currentIndex: navCubit.state,
+                    onTap: (value) {
+                      context
+                          .read<ChannelHome>()
+                          .isHome(value == 0 ? true : false);
+
+                      navCubit.switchNavBar(value);
+                    },
+                    backgroundColor: themes.background,
+                    selectedItemColor: themes.text,
+                    unselectedItemColor: themes.widget,
+                    items: [
+                      BottomNavigationBarItem(
+                          label: 'Home',
+                          icon: SvgPicture.asset(
+                            'assets/icons/open-book.svg',
+                            colorFilter: ColorFilter.mode(
+                                themes.widget, BlendMode.srcIn),
+                          )),
+                      // BottomNavigationBarItem(
+                      //     label: 'Listen',
+                      //     icon: SvgPicture.asset(
+                      //       'assets/icons/listen.svg',
+                      //       colorFilter: const ColorFilter.mode(
+                      //           Color(0xff1b5ed1), BlendMode.srcIn),
+                      //     )),
+                      BottomNavigationBarItem(
+                          label: 'Profile',
+                          icon: SvgPicture.asset(
+                            'assets/icons/account.svg',
+                            colorFilter: ColorFilter.mode(
+                                themes.widget, BlendMode.srcIn),
+                          ))
+                    ]),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: context.watch<NavbarCubit>().state == 0
+            ? const FloatingPdfAction()
+            : null,
       ),
-      floatingActionButton: context.watch<NavbarCubit>().state == 0
-          ? const FloatingPdfAction()
-          : null,
     );
   }
 }
